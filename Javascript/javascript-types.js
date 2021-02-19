44d76c768dd90aa818fa89f3ac4c9c30b9d92f8b
Jaren Cudilla <jarencudilla@gmail.com>
2021-02-19 21:26:04 +0800

    Create javascript-types.js

---
+++ Javascript/javascript-types.js
@@ -0,0 +1,37 @@
+/* var a;
+typeof a;
+
+a = "hello world";
+a = 42;
+typeof a;
+
+a = true;
+typeof a;
+
+a = null;
+typeof a;
+
+a = undefined;
+typeof a;
+
+a = { b: "c"};
+typeof a;
+
+typeof Symbol();
+
+var a = null;
+(!a && typeof === "object");
+*/
+//typeof function a(){/* .. */} === "function";
+//function a(b,c) {
+    /* .. */
+//}
+
+//a.length
+//typeof a;
+//console.log(typeof a);
+//console.log(a.length);
+
+var a = ( );
+var b = { };
+console.log(a);
